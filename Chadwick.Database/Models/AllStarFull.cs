6addc4667ed73df7e6323d9d6b1ee7efe0d707d0
Marc Wilson <mswilson4040@hotmail.com>
2020-09-26 20:09:15 -0700

    applied migration

--- Chadwick.Database/Models/AllStarFull.cs
+++ Chadwick.Database/Models/AllStarFull.cs
@@ -1,5 +1,3 @@
-using CsvHelper.Configuration.Attributes;
-
 namespace Chadwick.Database.Models
 {
     /// <summary>
@@ -7,23 +5,14 @@ namespace Chadwick.Database.Models
     /// </summary>
     public class AllStarFull
     {
-        [Ignore]
         public int Id { get; set; }
-        [Name("playerID")]
         public string PlayerId { get; set; }
-        [Name("yearID")]
         public int? YearId { get; set; }
-        [Name("gameNum")]
         public int? GameNumber { get;set; }
-        [Name("gameID")]
         public string GameId { get; set; }
-        [Name("teamID")]
         public string TeamId { get;set; }
-        [Name("lgID")]
         public string LeagueId { get;set; }
-        [Name("GP")]
         public int? GamesPlayed { get; set; }
-        [Name("startingPos")]
         public int? StartingPosition { get; set; }
     }
 }
